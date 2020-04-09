88470c961ab386a9c612dc9ac0d6b04974a524d3
BLHX <2819208060@qq.com>
2020-04-09 22:53:52 +0800

    login

--- src/main/java/net/xdclass/xdvideo/config/WeChatConfig.java
+++ src/main/java/net/xdclass/xdvideo/config/WeChatConfig.java
@@ -17,6 +17,50 @@ public class WeChatConfig {
      */
     @Value("${wxpay.appsecret}")
     private String appsecret;
+    /**
+     * 开放平台Appid
+     */
+    @Value("${wxopen.appid}")
+    private String openAppId;
+    /**
+     * 开放平台appsercret
+     */
+    @Value("${wxopen.appsecret}")
+    private String openAppSecret;
+    /**
+     * 开放平台回调url
+     */
+    @Value("${wxopen.redirect_url}")
+    private String openRedirectUrl;
+    /**
+     * 微信开放平台二维码连接
+     */
+    private final static String OPEN_QRCODE_URL= "https://open.weixin.qq.com/connect/qrconnect?appid=%s&redirect_uri=%s&response_type=code&scope=snsapi_login&state=%s#wechat_redirect";
+    public String getOpenAppId ( ){
+        return openAppId;
+    }
+    public static String getOpenQrcodeUrl ( ){
+        return OPEN_QRCODE_URL;
+    }
+    public void setOpenAppId ( String openAppId ){
+        this.openAppId=openAppId;
+    }
+
+    public String getOpenAppSecret ( ){
+        return openAppSecret;
+    }
+
+    public void setOpenAppSecret ( String openAppSecret ){
+        this.openAppSecret=openAppSecret;
+    }
+
+    public String getOpenRedirectUrl ( ){
+        return openRedirectUrl;
+    }
+
+    public void setOpenRedirectUrl ( String openRedirectUrl ){
+        this.openRedirectUrl=openRedirectUrl;
+    }
 
     public String getAppId ( ){
         return appId;
--- src/main/java/net/xdclass/xdvideo/controller/TestController.java
+++ src/main/java/net/xdclass/xdvideo/controller/TestController.java
@@ -1,13 +1,14 @@
 package net.xdclass.xdvideo.controller;
 
 import net.xdclass.xdvideo.config.WeChatConfig;
-import net.xdclass.xdvideo.domain.Video;
+
+import net.xdclass.xdvideo.domain.JsonData;
 import net.xdclass.xdvideo.mapper.VideoMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+
 
 
 @RestController
@@ -22,9 +23,9 @@ public class TestController {
 	@Autowired
 	private WeChatConfig weChatConfig;
 	@RequestMapping("test_config")
-	public String testConfig(){
+	public JsonData testConfig(){
 		System.out.println(weChatConfig.getAppId());
-		return "hello xdclass.net777";
+		return JsonData.buildSuccess(weChatConfig.getAppId());
 	}
 	@Autowired
 	private VideoMapper videoMapper;
